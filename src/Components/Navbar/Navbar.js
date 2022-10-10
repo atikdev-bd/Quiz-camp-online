2e2ca4605bb0665117cb19de6e5881e6c3887958
atikdev-bd <atikdev.bd@gmail.com>
2022-10-10 22:08:32 +0600

    navbar link color and hover effect added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,13 +7,13 @@ const Navbar = () => {
     <div>
       <nav className="bg-slate-600 flex justify-between px-16 h-14 items-center">
         <div>
-        <Link className="text-4xl font-bold">QUIZ CAMP</Link>
+        <Link className="text-4xl font-bold"><span className="text-amber-600 hover:text-yellow-400">QUIZ </span><span className="text-white hover:text-cyan-500">CAMP</span></Link>
         </div>
         <div className="link">
-          <Link className="ml-3 text-xl font-semibold">Home</Link>
-          <Link className="ml-3 font-semibold">Topics</Link>
-          <Link className="ml-3 font-semibold">Statistics</Link>
-          <Link className="ml-3 font-semibold">Blog</Link>
+          <Link className="ml-3 text-xl font-semibold text-stone-200 hover:text-sky-400">Home</Link>
+          <Link className="ml-3 font-semibold text-cyan-300 hover:text-yellow-200">Topics</Link>
+          <Link className="ml-3 font-semibold text-cyan-300 hover:text-yellow-200">Statistics</Link>
+          <Link className="ml-3 font-semibold text-cyan-300 hover:text-yellow-200">Blog</Link>
         </div>
       </nav>
     </div>
